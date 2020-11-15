c22a8967bfbc68f136faec63b5a7ad74b8c23ad2
rodricguifo <52127611+rodricguifo@users.noreply.github.com>
2020-11-15 09:01:19 +0100

    second commit

--- README.md
+++ README.md
@@ -1,15 +1,48 @@
-Getting and Cleaning Data
-=========================
+# Background Peer graded Assignment Getting and Cleaning Data Course Project Oct-2016
 
-This is a repository for any and all code written for the Getting and Cleaning Data Coursera course through Johns Hopkins University.  
+##Getting and Cleaning Data - peer assessment project
 
-## Course Project
-(find all project-related materials in the UCI HAR Dataset directory, however, copies of the important files have been put into this main directory to fulfill the submission requirement)  
+The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. You will be graded by your peers on a series of yes/no questions related to the project. You will be required to submit: 1) a tidy data set as described below, 2) a link to a Github repository with your script for performing the analysis, and 3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.
 
-* Unzip the source (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) into a folder on your local drive, say C:\Users\yourname\Documents\R\  
+One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
 
-* Put run_analysis.R into C:\Users\yourname\Documents\R\UCI HAR Dataset\  
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
-* In RStudio: setwd("C:\\\\Users\\\\yourname\\\\Documents\\\\R\\\\UCI HAR Dataset\\\\"), followed by: source("run_analysis.R")  
+Here are the data for the project:
 
-* Use data <- read.table("data_set_with_the_averages.txt") to read the data. It is 180x68 because there are 30 subjects and 6 activities, thus "for each activity and each subject" means 30 * 6 = 180 rows. Note that the provided R script has no assumptions on numbers of records, only on locations of files.  
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+
+You should create one R script called run_analysis.R that does the following.
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+#In the run_analysis.R script,  were recreated  each step.
+
+This file 'run_analysis.R' contains all the code to perform the analyses described in the 5 steps. They can be launched in RStudio by just importing the file.
+
+## About this R script
+File with R code "run_analysis.R" perform 5 following steps (in accordance assigned task of course work):   
+1. Merging the training and the test sets to create one data set.   
+  1.1 Reading files    
+    1.1.1 Reading trainings tables   
+    1.1.2 Reading testing tables   
+    1.1.3 Reading feature vector   
+    1.1.4 Reading activity labels   
+  1.2 Assigning column names   
+  1.3 Merging all data in one set   
+2. Extracting only the measurements on the mean and standard deviation for each measurement   
+  2.1 Reading column names  
+  2.2 Create vector for defining ID, mean and standard deviation   
+  2.3 Making nessesary subset from setAllInOne   
+3. Using descriptive activity names to name the activities in the data set   
+4. Appropriately labeling the data set with descriptive variable names   
+5. Creating a second, independent tidy data set with the average of each variable for each activity and each subject   
+  5.1 Making second tidy data set   
+  5.2 Writing second tidy data set in txt file   
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,59 +1,105 @@
-# Source of data for this project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#******************************************************************
+#Step 0. Downloading and unzipping dataset
+#******************************************************************
 
-# This R script does the following:
+if(!file.exists("./data")){dir.create("./data")}
+#Here are the data for the project:
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
 
-# 1. Merges the training and the test sets to create one data set.
+# Unzip dataSet to /data directory
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
-temp_train <- read.table("train/X_train.txt")
-temp_test <- read.table("test/X_test.txt")
-X <- rbind(temp_train, temp_test)
+#You should create one R script called run_analysis.R that does the following.
 
-temp_train <- read.table("train/y_train.txt")
-temp_test <- read.table("test/y_test.txt")
-Y <- rbind(temp_train, temp_test)
+#******************************************************************
+#Step 1.Merges the training and the test sets to create one data set.
+#******************************************************************
 
-temp_train <- read.table("train/subject_train.txt")
-temp_test <- read.table("test/subject_test.txt")
-Subject <- rbind(temp_train, temp_test)
+# 1.1 Reading files
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 1.1.1  Reading trainings tables:
 
-features <- read.table("features.txt")
-index_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
-X <- X[, index_of_good_features]
-names(X) <- features[index_of_good_features, 2]
-names(X) <- gsub("\\(|\\)", "", names(X))
-names(X) <- tolower(names(X))
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-# 3. Uses descriptive activity names to name the activities in the data set.
+# 1.1.2 Reading testing tables:
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-activity <- read.table("activity_labels.txt")
-activity[, 2] = gsub("_", "", tolower(as.character(activity[, 2])))
-Y[, 1] = activity[Y[ , 1], 2]
-names(Y) <- "activity"
+# 1.1.3 Reading feature vector:
+features <- read.table('./data/UCI HAR Dataset/features.txt')
 
-# 4. Appropriately labels the data set with descriptive activity names.
+# 1.1.4 Reading activity labels:
+activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
 
-names(Subject) <- "subject"
-clean <- cbind(Subject, Y, X)
-write.table(clean, "merged_clean_and_tidy_data.txt")
+# 1.2 Assigning column names:
 
-# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+colnames(x_train) <- features[,2]
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
 
-uniqueSubjects <- unique(Subject)[, 1]
-numSubjects <- length(unique(Subject)[, 1])
-numActivities <- length(activity[, 1])
-numColumns <- dim(clean)[2]
-result <- clean[1:(numSubjects*numActivities), ]
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
 
-row <- 1
-for (s in 1:numSubjects){
-        for (a in 1:numActivities){
-                result[row, 1] <- uniqueSubjects[s]
-                result[row, 2] <- activity[a, 2]
-                temp <- clean[clean$subject == s & clean$activity == activity[a, 2], ]
-                result[row, 3:numColumns] <- colMeans(temp[, 3:numColumns])
-                row <- row + 1
-        }
-}
-write.table(result, "data_set_with_the_averages.txt")
+colnames(activityLabels) <- c('activityId','activityType')
+
+#1.3 Merging all data in one set:
+
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
+
+#dim(setAllInOne)
+#[1] 10299   563
+
+#******************************************************************
+#Step 2.-Extracts only the measurements on the mean and standard deviation for each measurement.
+#******************************************************************
+
+#2.1 Reading column names:
+
+colNames <- colnames(setAllInOne)
+
+#2.2 Create vector for defining ID, mean and standard deviation:
+
+mean_and_std <- (grepl("activityId" , colNames) | 
+                   grepl("subjectId" , colNames) | 
+                   grepl("mean.." , colNames) | 
+                   grepl("std.." , colNames) 
+)
+
+#2.3 Making nessesary subset from setAllInOne:
+
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+#******************************************************************
+#Step 3. Uses descriptive activity names to name the activities in the data set
+#******************************************************************
+
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+
+#******************************************************************
+#Step 4. Appropriately labels the data set with descriptive variable names.
+#******************************************************************
+
+#Done in previous steps, see 1.3,2.2 and 2.3!
+
+#******************************************************************
+#Step 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#******************************************************************
+
+#5.1 Making a second tidy data set
+
+secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+
+#5.2 Writing second tidy data set in txt file
+
+write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
+
